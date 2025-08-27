607d73d891ad8eeac9bf1a4e6e2250bbe141358e
diazkimberly921-creator <diazkimberly921@gmail.com>
2025-08-27 15:34:15 +0800

    firebase configuration

---
+++ firebaseConfig.js
@@ -0,0 +1,13 @@
+
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCUPsp0ngAk-v_s0VjkUrgRBv9Cx3P5EkE",
+  authDomain: "ja-goals.firebaseapp.com",
+  projectId: "ja-goals",
+  storageBucket: "ja-goals.firebasestorage.app",
+  messagingSenderId: "930689276336",
+  appId: "1:930689276336:web:c583912a21c2056c4b9e75"
+};
+
+const app = initializeApp(firebaseConfig);
